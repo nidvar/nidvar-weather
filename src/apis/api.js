d4b650b5f62393ca7f0ffe01b13930e4d722a238
Nidvar <djayshine87@hotmail.com>
2021-01-03 17:26:22 +1100

    weather without redux

---
+++ src/apis/api.js
@@ -0,0 +1,6 @@
+let x = `http://api.weatherstack.com/current
+? access_key = 
+& query = New York`
+
+
+`https://jsonplaceholder.typicode.com/posts`
---
+++ src/components/weather.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Weather extends React.Component{
+    state={
+        data:[]
+    }
+    render(){
+        return(
+            <div>weather</div>
+        )
+    }
+}
+
+export default Weather

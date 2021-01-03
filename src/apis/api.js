06afd9de400e31d2620b3c5eb87e60176feccae0
Nidvar <djayshine87@hotmail.com>
2021-01-03 19:19:42 +1100

    displaying 5 cities working

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,6 @@
-let x = `http://api.weatherstack.com/current
-? access_key = 
-& query = New York`
+// let x = `http://api.weatherstack.com/current?access_key=af12ca9a089be84523c7c7148f49f2d8&query=`
+
+// let x = `https://api.mapbox.com/geocoding/v5/mapbox.places/${}.json?access_token=pk.eyJ1IjoibmlkdmFyY2xvdWQiLCJhIjoiY2tqZ3NhZWlsMHhmZzJ0bXM2a21kMzJ2dCJ9.HgkpS4IPHCtSv03I_ECI8A`
 
 
-`https://jsonplaceholder.typicode.com/posts`
+// export default x
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,11 +2,51 @@ import React from 'react';
 
 class Weather extends React.Component{
     state={
-        data:[]
+        data:[],
+        target:'',
+        timer:null
+    }
+    componentDidMount(){
+        this.setState({timer:null})
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log('test')
+    }
+    handleChange=(e)=>{
+        this.setState({
+            target: e.target.value
+        })
+        clearTimeout(this.state.timer);
+        this.setState({timer:setTimeout(()=>{
+            this.fetch_geocodes()
+        },2000)})
+    }
+    fetch_geocodes = ()=>{
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.target}.json?access_token=`).then(a=>{
+            return a.json()
+        }).then(a=>{
+            this.setState({
+                data: a.features
+            })
+        }).catch(e=>{console.log(e)})
+    }
+    display_location=()=>{
+        return this.state.data.map(a=>{
+            return (<div key={a.id}><h2>{a.place_name}</h2></div>)
+        })
     }
     render(){
+        console.log(this.state)
         return(
-            <div>weather</div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange}/>
+                </form>
+                <div className='my_dropdown'>
+                    {this.display_location()}
+                </div>
+            </div>
         )
     }
 }

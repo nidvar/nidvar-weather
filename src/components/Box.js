9e0ab6593156dd8f9057016e704b4b34572b6246
Nidvar <djayshine87@hotmail.com>
2021-01-05 10:17:45 +1100

    finished

--- src/components/Box.js
+++ src/components/Box.js
@@ -10,8 +10,8 @@ import partlycloudy from '../images/partlycloudy.png'
 import haze from '../images/haze.png'
 
 const Box = (props)=>{
-    const pics = [partlycloudy, snow, rain, mist ,thunder, sunny, cloudy, haze, snow, cloudy, snow]
-    const sources = ['partly cloudy', 'snow', 'rain', 'mist' ,'thunder', 'sunny', 'cloudy', 'haze', 'sleet', 'overcast', 'blizzard']
+    const pics = [partlycloudy, snow, rain, mist ,thunder, sunny, cloudy, haze, snow, cloudy, snow, sunny]
+    const sources = ['partly cloudy', 'snow', 'rain', 'mist' ,'thunder', 'sunny', 'cloudy', 'haze', 'sleet', 'overcast', 'blizzard', 'clear']
     const value = props.data_value[0].toLowerCase();
     const result = ()=>{
         for(let i =0; i<=pics.length-1; i++){
--- src/components/weather.js
+++ src/components/weather.js
@@ -7,10 +7,11 @@ class Weather extends React.Component{
         data:[],
         target:'',
         timer:null,
-        token:``,
+        token:`..`,
         key: '',
         show_results:false,
-        selected_city:''
+        selected_city:'',
+        home:true
     }
     componentDidMount(){
         this.setState({timer:null})
@@ -20,7 +21,8 @@ class Weather extends React.Component{
         this.fetch_geocodes()
         this.setState({
             target:'',
-            show_results:false
+            show_results:false,
+            home:false
         })
     }
     handleChange=(e)=>{
@@ -58,11 +60,23 @@ class Weather extends React.Component{
             }).catch(e=>{console.log(e)})
         }
     }
+    home=()=>{
+        this.setState({
+            home:true
+        })
+    }
     display_location=()=>{
-        if(this.state.show_results == false){
-            if(this.state.data == undefined){
-                return null
-            }else{
+        if(this.state.home == true){
+            return (
+                <div className='hometext'>
+                    <h2>About this app</h2>
+                    <p>Uses 3rd Party API's to generate weather forcasts of a desired location.</p>
+                    <p>When the search button is clicked, the Mapbox API will generate longitude and latitude of the top 5 locations matching the search term.</p>
+                    <p>After selecting a location, the Weatherstack API will return the appropriate forcast.</p>
+                </div>
+            )
+        }else{
+            if(this.state.show_results == false){
                 return this.state.data.map(a=>{
                     return (
                         <p className='locationname'key={a.id} onClick={()=>{this.handleClick(a.center[0], a.center[1])}}>
@@ -71,26 +85,29 @@ class Weather extends React.Component{
                     )
                 })
             }
-        }
-        if(this.state.show_results==true){
-            const x = this.state.selected_city.current
-            return (
-                <div>
-                    <Box data={'Description'} data_value={x.weather_descriptions} temperature = {x.temperature} />
-                    <div className='stats'>
-                        <div className='statsbox'>
-                            <p><span className='results'>Humidity: </span>{x.humidity}%</p>
-                            <p><span className='results'>Precipitation: </span>{x.precip}%</p>
+            if(this.state.show_results==true){
+                const x = this.state.selected_city.current
+                return (
+                    <div>
+                        <Box data={'Description'} data_value={x.weather_descriptions} temperature = {x.temperature} />
+                        <div className='stats'>
+                            <div className='statsbox'>
+                                <p><span className='results'>Humidity: </span>{x.humidity}%</p>
+                                <p><span className='results'>Precipitation: </span>{x.precip}%</p>
+                            </div>
+                            <div className='statsbox'>
+                                <p><span className='results'>Wind speed: </span>{x.wind_speed}km/h</p>
+                                <p><span className='results'>Observation Time: </span>{x.observation_time}</p>
+                            </div>
                         </div>
-                        <div className='statsbox'>
-                            <p><span className='results'>Wind speed: </span>{x.wind_speed}km/h</p>
-                            <p><span className='results'>Observation Time: </span>{x.observation_time}</p>
-                        </div>
+                        <br />
+                        <br />
+                        <button className="btn btn-primary homebutton" type="button" onClick={this.home}>HOME</button>
                     </div>
-
-                </div>
-            )
+                )
+            }
         }
+
     }
     render(){
         return(
